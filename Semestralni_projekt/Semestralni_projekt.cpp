4dc323898caf5cc2c4a9cb091b00fd0210e3e42c
nadvornikd <fenix@LAPTOP-SI5S6M70>
2023-11-06 21:26:21 +0100

    Add project files.

---
+++ Semestralni_projekt/Semestralni_projekt.cpp
@@ -0,0 +1,12 @@
+﻿// Semestralni_projekt.cpp : Defines the entry point for the application.
+//
+
+#include "Semestralni_projekt.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
